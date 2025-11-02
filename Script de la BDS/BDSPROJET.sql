ed4c0cb53b7305ba559984e49adfa30934005501
salwaafroukh <salwaafroukh@gmail.com>
2025-11-02 21:30:53 +0100

    Premier commit avec LFS

---
+++ Script de la BDS/BDSPROJET.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3b01a1eff6efa75cb5e547f10cdf547d36f0dff51e9f93630b1818a6eaed3a2
+size 720073057
---
+++ Script de la BDS/README.txt
@@ -0,0 +1,2 @@
+﻿Pour exporter la base exécutez cette commande dans psql ou Git Bach 
+psql -U postgres -d nombase -f "chemainfichiersql"
